23c0ac813f49f96b0f8f79c785959d9e46e5c426
Alexey Dudchenko <alexey.Dudchenko@gmail.com>
2016-03-18 13:56:26 +0600

    Added files via upload
    Задача свелась к созданию формы для заполнения Excel таблицы

---
+++ CreditApp/DebitWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Office.Interop.Excel;
+using Window = System.Windows.Window;
+
+namespace CreditApp
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class DebitWindow : Window
+    {
+        private int lastrow = 0;
+
+        public DebitWindow()
+        {
+            InitializeComponent();
+
+
+            // открываем документ и лист для считывания данных для comboBox
+            Microsoft.Office.Interop.Excel.Application excelApp = new Microsoft.Office.Interop.Excel.Application();
+            Microsoft.Office.Interop.Excel.Workbook workbook =
+                excelApp.Workbooks.Open(Environment.CurrentDirectory + "\\Material4.xlsx");
+            Microsoft.Office.Interop.Excel.Worksheet worksheet =
+                (Microsoft.Office.Interop.Excel.Worksheet) workbook.Sheets["Mat"];
+
+
+            // заполняем comboBox значениями
+            for (int i = 3; i < 76; i++)
+            {
+                MaterialComboBox.Items.Add(worksheet.Cells[i, 2].Value);
+            }
+
+            // получем номер последнего документа
+            worksheet = (Microsoft.Office.Interop.Excel.Worksheet) workbook.Sheets["Расход материалов"];
+            Microsoft.Office.Interop.Excel.Range range = worksheet.UsedRange;
+
+            DocNamberTexBox.Text = worksheet.Cells[2, 2].Value;
+
+            // закрываем Excel
+            workbook.Close(true, Missing.Value, Missing.Value);
+            excelApp.Quit();
+
+
+            // заполняем текущее время
+            DateTexBox.Text = DateTime.Now.ToString("dd.MM.yyyy");
+
+        }
+
+        private void Button_Click_Add(object sender, RoutedEventArgs e)
+        {
+
+            Microsoft.Office.Interop.Excel.Application excelApp = new Microsoft.Office.Interop.Excel.Application();
+            Microsoft.Office.Interop.Excel.Workbook workbook =
+                excelApp.Workbooks.Open(Environment.CurrentDirectory + "\\Material4.xlsx");
+            Microsoft.Office.Interop.Excel.Worksheet worksheet =
+                (Microsoft.Office.Interop.Excel.Worksheet) workbook.Sheets["Расход материалов"];
+            Microsoft.Office.Interop.Excel.Range range = worksheet.UsedRange;
+
+            int column = 0;
+
+            //Получаем номер последней заполненной строки
+            int lastrow = worksheet.UsedRange.Rows.Count;
+
+
+
+
+            // проверям совпадение номера документа
+            Range cellRange = (Range) worksheet.Cells[lastrow - 1, 3];
+            string cellValue = cellRange.Value.ToString();
+
+            if (cellValue == DocNamberTexBox.Text)
+            {
+                worksheet.Cells[lastrow - 1, MaterialComboBox.SelectedIndex + 4] = CreditMaterialTextBox.Text;
+            }
+            // добавляем новую строку ели номер не совпал
+            else
+            {
+                for (column = 3; column <= range.Columns.Count; column++)
+                {
+                    worksheet.Cells[lastrow + 1, column] = worksheet.Cells[lastrow, column];
+                    worksheet.Cells[lastrow, column] = "";
+                }
+                // заполняем заполняем ячейки файла данными
+                worksheet.Cells[lastrow, 1] = lastrow - 2;
+                worksheet.Cells[lastrow, 2] = DateTexBox.Text;
+                worksheet.Cells[lastrow, 3] = DocNamberTexBox.Text;
+                worksheet.Cells[lastrow, MaterialComboBox.SelectedIndex + 4] = CreditMaterialTextBox.Text;
+                // в ячейку девятого столбца вводим формулу для подсчета суммы. Таким образом храним в файле не сумму, а заставляем Excel её считать самостоятельно
+                //worksheet.Cells[lastrow + 1, 3] = ("Сумма");  // ("=C" + lastrow + "-F" + lastrow);
+                //Стираем все с последней строки. Потом запишем это заново в конец
+
+            }
+
+
+
+
+            // закрываем Excel
+            workbook.Close(true, Missing.Value, Missing.Value);
+            excelApp.Quit();
+
+            // выводим информационно сообщение
+            MessageBox.Show("Добавлен расход материала " + MaterialComboBox.Text + " в размере " +
+                            CreditMaterialTextBox.Text);
+
+            MaterialComboBox.Text = "выберите материал"; // так не работает ))
+            CreditMaterialTextBox.Text = String.Empty;
+
+        }
+
+        private void Button_Click_Exit(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+       
+
+    }
+}
---
+++ CreditApp/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Office.Interop.Excel;
+using Window = System.Windows.Window;
+
+namespace CreditApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // заполняем текущее время         
+            Datelable.Content = Convert.ToString("Текущая дата: " + DateTime.Now.ToString("dd.MM.yyyy"));
+
+        }
+
+
+        private void Button_Click_Debit_Material(object sender, RoutedEventArgs e)
+        {
+            Window debitWindow = new DebitWindow();
+            debitWindow.Show();
+        }
+
+        private void Button_Click_Credit_Material(object sender, RoutedEventArgs e)
+        {
+            Window creditWindow = new CreditWindow();
+            creditWindow.Show();
+        }
+
+        private void Button_Click_Credit_Maney(object sender, RoutedEventArgs e)
+        {
+            Window creditWindow = new CreditWindow();
+            creditWindow.Show();
+        }
+
+        private void Button_Click_Debit_Maney(object sender, RoutedEventArgs e)
+        {
+            Window creditWindow = new CreditWindow();
+            creditWindow.Show();
+        }
+
+
+
+
+
+    }
+}
